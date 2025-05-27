7b7312fe1c83bc5ca2ecd8325c7eecdbafd4f515
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-05-26 17:29:03 -0700

    initial commit

---
+++ methods.rb
@@ -0,0 +1,27 @@
+#player 
+  #attributes
+    #name
+    #lives
+  #methods
+    #lose_life
+    #alive?^^^
+#question 
+  #attributes
+    #num1
+    #num2
+    #answer
+  #methods
+   #generate_question
+   #check_answer?
+#math_game
+  #attributes
+    #players
+    #current_player
+  #methods
+    #start
+    #next_turn
+    #check_winner
+    #switch_player
+    #game_over?
+
+    
